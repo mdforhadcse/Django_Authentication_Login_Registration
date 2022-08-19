2260fc8fe3298b12c68bd76da1a19331afe1b026
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-08-19 23:03:10 +0600

    Login system

--- Django_Authentication_Login_Registration/settings.py
+++ Django_Authentication_Login_Registration/settings.py
@@ -117,6 +117,9 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 MEDIA_URL = "media/"
 MEDIA_ROOT = (BASE_DIR / "media",)
 
+# Login
+LOGIN_URL = '/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- Login_app/views.py
+++ Login_app/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 from Login_app.forms import UserForm, UserInfoFrom
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 
 # Create your views here.
+def login_page(request):
+    return render(request, 'Login_app/login.html', context={})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('Login_app:index'))
+            else:
+                return HttpResponse("Account is not active!!!")
+        else:
+            return HttpResponse("Login credential is wrong!!!")
+    else:
+        return HttpResponseRedirect(reverse('Login_app:login'))
+
+
+@login_required()
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('Login_app:index'))
+
+
 def index(request):
     cont = {}
     return render(request, 'Login_app/index.html', context=cont)
@@ -17,7 +51,7 @@ def register(request):
 
         if user_form.is_valid() and user_info_form.is_valid():
             user = user_form.save()
-            user.set_password(user.password)    # password encryption
+            user.set_password(user.password)  # password encryption
             user.save()
 
             user_info = user_info_form.save(commit=False)
